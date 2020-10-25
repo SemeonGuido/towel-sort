3dbfc71fcc3078da2e9505170e3ed95b79b058b7
Semeon Guido <Semeon.Guido@gmail.com>
2020-10-25 21:52:48 +0300

    choose task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix == undefined || matrix.length == 0) return [];
+    let result = [];
+    let reverse = false;
+    matrix.forEach(function (elem) {
+        if (!reverse) {
+            elem.forEach(function (one) {
+                result.push(one);
+            });
+            reverse = true;
+        } else {
+            elem.reverse().forEach(function (one) {
+                result.push(one);
+            });
+            reverse = false;
+        }
+    });
+
+    return result;
 }
